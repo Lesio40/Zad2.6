a0bfe5794efbae9fdad843a1726cbc3ed7ae8c6a
Lesio40 <126687528+Lesio40@users.noreply.github.com>
2023-03-15 14:34:19 +0000

    Initial commit

---
+++ Main.java
@@ -0,0 +1,11 @@
+class Main {
+  public static void main(String[] args) {
+    int c,f;
+    for(int i=0;i<=300;i=i+20)
+      {
+        c=i;
+        f=(2*c)+32;
+        System.out.println("Celszjusz= " + c + " Farenhajt= " + f);
+      }
+  }
+}
